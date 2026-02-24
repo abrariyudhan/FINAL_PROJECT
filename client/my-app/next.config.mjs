2435c5a935fed7a780ed31d48dd904b848a5db2d
RILMANIA <emir.haikal.eh@gmail.com>
2026-02-24 14:28:03 +0700

    tambah fungsi upload file ke chat

--- client/my-app/next.config.mjs
+++ client/my-app/next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // Increase body size limit for file uploads
+    },
+  },
 };
 
 export default nextConfig;
